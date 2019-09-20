52eefbc527bb22ac34b53cfa2a9cc0d6eb47c3e9
Szűcs Richárd <szucs.richard@vereb.dc>
2019-09-20 16:39:17 +0200

    Add project files.

---
+++ Valami/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Valami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, hogy hívnak?");
+            var v = Console.ReadLine();
+            Console.WriteLine($"Milyen szép neved van {v}");
+            Console.WriteLine("Szeretsz programozni?");
+            v = Console.ReadLine();
+            if (v == "igen")
+                Console.WriteLine("jaj de jó");
+            else
+                Console.WriteLine("Nem jó");
+
+            Console.ReadKey();
+        }
+    }
+}
